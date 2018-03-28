817f084b601649f94fcd83dbea5f3d27808c244e
patel <cp425@njit.edu>
2018-03-28 01:28:18 -0400

    3 properties to it done

---
+++ database/factories/CarFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Car::class, function (Faker $faker) {
+    return [
+        'make' => $faker->name,
+        'model' => $faker->name,
+        'year' => $faker->name,
+    ];
+});
