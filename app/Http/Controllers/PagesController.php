ff6782d04ee80cb1c97fd8230a5541a53ff34a41
patel <cp425@njit.edu>
2018-03-29 11:12:13 -0400

    About page setup for heroku

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    function index()
+    {
+        return view('pages.index');
+    }
+
+    function contact()
+    {
+        return view('pages.contact');
+    }
+    function about()
+    {
+        return view('pages.about');
+    }
+
+
+}
