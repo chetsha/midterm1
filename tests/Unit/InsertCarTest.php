2e3b79d4cf059f8b48322e1469c1cd1d0f5824c1
patel <cp425@njit.edu>
2018-03-29 14:00:52 -0400

    Inser Car test done

---
+++ tests/Unit/InsertCarTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+class InsertCarTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testInsertCar()
+    {
+        $car = new Car();
+        $car->Make = 'Ford';
+        $car->Model = 'canada';
+        $car->year = '1978';
+
+        $this->assertTrue($car->save());
+    }
+}
